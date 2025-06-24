b80b9ee32f7b1563a8e79881b6971968f09a3799
Kshh1423 <kshkaungsetthlaing@gmail.com>
2025-06-24 20:40:41 +0630

    Create iszero.py

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+#enter number "0" and "non zero numbers"
+number = int(input("pls enter the number:"))
+if number == 0 :
+    print("this number is equal to zero")
+elif number != 0 :
+    print("This number is different from zero")
