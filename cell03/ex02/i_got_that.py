7f57237c2a9f1c2c70df97c413f6897ef8ffa507
Kshh1423 <kshkaungsetthlaing@gmail.com>
2025-06-25 11:37:48 +0630

    Create  i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr?bin/env python3
+user_input = ""
+while user_input != "STOP" :
+    if user_input == "" :
+        user_input = input("What you gotta say:")
+    else :
+        user_input = input("I got that! Anything else ? :")
