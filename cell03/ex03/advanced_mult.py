30cd07e716bd70a2ffb5e6d158cf8934092e328a
Kshh1423 <kshkaungsetthlaing@gmail.com>
2025-06-25 21:29:25 +0630

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-i = 0
-while i <= 10:
-    print(f"Table de {i}:", end=" ")
-    j = 0
-    while j <= 10:
-        print(i * j, end=" " if j < 10 else "")
-        j += 1
-    print()
-    i += 1
+import sys
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        line = f"Table de {i}:"
+        j = 0
+        while j <= 10:
+            line += f" {i * j}"
+            j += 1
+        print(line)
+        i += 1
