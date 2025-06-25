ca69fd026d2ca73a5ab52674bdd2230dee7f8552
Kshh1423 <kshkaungsetthlaing@gmail.com>
2025-06-25 10:01:17 +0630

    Create to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+input_password = int(input(" Enter a number less than 25:"))
+print(input_password)
+if input_password > 25 :
+    print("Error")
+count = input_password
+while count < 25:
+    print(f" Inside the loop, my variable is {count}")
+    count += 1
